53d0e99fe5fa31b70555ab0d971bffddc3edc392
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2016-07-16 22:47:33 +0900

    For redmine 3.3.0

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,7 +1,9 @@
-document.observe('dom:loaded', function() {
-    if ($('main-menu')) {
-        return;
+$(document).ready(function() {
+    if (!$('#main-menu').length > 0) {
+        $('#main').addClass('no-main-menu');
     }
-    $('main').addClassName('no-main-menu');
+    if ($('.mobile-toggle-button').css('display') != 'none') {
+      $('#main').addClass('no-main-menu');
+      $('body').addClass('mobile-mode');
+    }
 });
-
